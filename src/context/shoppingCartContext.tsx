7da4c7e0d89bf6f110034ba8054305c368477c59
Juan Carlos Nuñez <juancarlosm.nunez@gmail.com>
2023-09-15 17:21:44 -0300

    finished the two functions for the shoppingCartContext

--- src/context/shoppingCartContext.tsx
+++ src/context/shoppingCartContext.tsx
@@ -1,1 +1,19 @@
-function
+import { ReactNode, createContext, useContext } from "react";
+
+const ShoppingCartContext = createContext({});
+
+export function useShopingCart(){
+    return useContext(ShoppingCartContext)
+}
+type ShoppingCartProviderProps{
+    children: ReactNode  /// is the type given to every react childre property
+}
+
+export function ShoppingCartProvider({children}: ShoppingCartProviderProps){
+    return(
+        <ShoppingCartContext.Provider value={{}}>
+            {children}
+        </ShoppingCartContext.Provider>
+    )
+
+}
