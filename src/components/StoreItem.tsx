c68bb4beed786da06922a63f903f4d26e396deca
Juan Carlos Nuñez <juancarlosm.nunez@gmail.com>
2023-09-15 16:46:08 -0300

    put / changed "first row" for +/-  buttons

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -29,8 +29,9 @@ export function StoreItem ({ id, name, price, imgUrl }: StoreItemProps)
                     <div className="d-flex align-items-center flex-column" style={{gap:"0.5rem"}} >
                         <div className="d-flex align-items-center justify-content-center"
                          style={{gap:".5rem"}}>
-                            first row
-                         </div>
+                        <Button>+</Button>
+                        <Button>-</Button>
+                        </div>
                           second row
                     </div> 
 }
