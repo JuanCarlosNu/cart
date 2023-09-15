fd6d00346e1fb02fe9f37da35c5cef07dd960f81
Juan Carlos Nuñez <juancarlosm.nunez@gmail.com>
2023-09-15 14:37:09 -0300

    repleced the Json.stringify method for the  StoreItem component

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,5 +1,6 @@
 import { Col, Row } from "react-bootstrap";
 import storeITems from "../data/items.json";
+import { StoreItem } from "../components/StoreItem";
 
 export function Store() {
     return ( 
@@ -7,7 +8,7 @@ export function Store() {
        <h1>Store</h1>
        <Row  md={2} xs={1} lg={3} className="g-3">
         {storeITems.map(item =>(
-            <Col>{JSON.stringify(item)}</Col>
+            <Col><StoreItem /></Col>
         ))}
        </Row>
        </>
