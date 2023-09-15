0b48b5c71574c3b6113e5c390731a30532311b95
Juan Carlos Nuñez <juancarlosm.nunez@gmail.com>
2023-09-15 12:35:32 -0300

    changed json syntax(no coma in last prop) and maped the info in bootstrap row and col

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,8 +1,17 @@
+import { Col, Row } from "react-bootstrap";
 import storeITems from "../data/items.json";
 
 export function Store() {
     return ( 
-        <div>Store</div>
+        <>   
+       <h1>Store</h1>
+       <Row>
+        {storeITems.map(item =>(
+            <Col>{JSON.stringify(item)}</Col>
+        ))}
+       </Row>
+       </>
+ 
      )
-};
+}
  
