e7c4d782e17f97ca4f720cbf8452711f013509c9
Juan Carlos Nuñez <juancarlosm.nunez@gmail.com>
2023-09-15 15:56:22 -0300

    corrected some warmings erros about the svg. diferences betwenn react and html css. uses camelcase instead of "-".

--- src/utilities/formatCurrency.ts
+++ src/utilities/formatCurrency.ts
@@ -1,6 +1,5 @@
 const CURRENCY_FORMATTER = new Intl.NumberFormat(undefined,{currency: "USD", style="currency"});
 
 export function formatCurrency(number: Number){
-
-
+return CURRENCY_FORMATTER.format(number);
 }
