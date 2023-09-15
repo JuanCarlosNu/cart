6c4a60d71f2f935ec40b9a0b5a727fa82d3bcbcd
Juan Carlos Nuñez <juancarlosm.nunez@gmail.com>
2023-09-15 15:00:55 -0300

    layout all the items images using bootstrap card tag. + style images to look all the same.

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,9 +1,22 @@
+import { Card } from "react-bootstrap"
+
+
 type StoreItemProps ={
     id: number
     name: string
     price: number
     imgUrl: string
 }
-export function StoreItem ({ id, name, price, imgUrl }: StoreItemProps){
+export function StoreItem ({ id, name, price, imgUrl }: StoreItemProps)
+{
+    return(
+        <Card>
+            <Card.Img variant="top" 
+                      src={imgUrl}
+                        height="200px" 
+                     style={{objectFit:"cover"}} // fix the aspect ratio
+            />
+        </Card>
+    )
 
 }
