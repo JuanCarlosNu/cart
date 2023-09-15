4cd8072b34b6ce0eb588d3c8d4f20d132b2a960c
Juan Carlos Nuñez <juancarlosm.nunez@gmail.com>
2023-09-15 15:10:07 -0300

    layout the line of the title and price of the card element.

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -14,8 +14,15 @@ export function StoreItem ({ id, name, price, imgUrl }: StoreItemProps)
             <Card.Img variant="top" 
                       src={imgUrl}
                         height="200px" 
-                     style={{objectFit:"cover"}} // fix the aspect ratio
+                     style={{objectFit:"cover"}} // fix the aspect Ratio
             />
+            <Card.Body className="d-flex flex-columg">
+                <Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
+                    <span className="fs-2">{name}</span>
+                    <span className="ms-2 text-muted">{price}</span>
+                </Card.Title>
+
+            </Card.Body>
         </Card>
     )
 
