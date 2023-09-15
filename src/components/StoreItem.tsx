816a2a1c1223701e82278260e1a6148909e89f0b
Juan Carlos Nuñez <juancarlosm.nunez@gmail.com>
2023-09-15 16:03:49 -0300

    Used the formatCurrency utility for the price of our sotreItem layout.

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -20,7 +20,7 @@ export function StoreItem ({ id, name, price, imgUrl }: StoreItemProps)
             <Card.Body className="d-flex flex-columg">
                 <Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
                     <span className="fs-2">{name}</span>
-                    <span className="ms-2 text-muted">{price}</span>
+                    <span className="ms-2 text-muted">{formatCurrency(price)}</span>
                 </Card.Title>
 
             </Card.Body>
