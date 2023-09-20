c955e10063ff70ef64c656967d5e67038f15e3e0
Juan Carlos Nuñez <juancarlosm.nunez@gmail.com>
2023-09-20 13:45:16 -0300

    corrected some styling issue related to the markup

--- src/components/cartItem.tsx
+++ src/components/cartItem.tsx
@@ -15,27 +15,35 @@ export function CartItem ({ id , quantity }: CartItemProps) {
         if(item == null) return null 
     
     return (
-<Stack direction="horizontal" gap={2}  className="d-flex align-items-center"> 
+<Stack direction="horizontal" gap={2}  className="d-flex 
+align-items-center"> 
 <img 
     src={item.imgUrl}
     style={{width: "125px", height: "75px", objectFit: "cover"}}
 />
 <div className="me-auto"> 
-<div>{item.name}  {quantity >1 && <span
-className="text-muted" style={{fontSize: "0.65rem"}}> X {quantity}  </span>}
-</div>
+      <div>
+        {item.name} {" "}  {quantity >1 && ( 
+        <span className="text-muted" style={{fontSize: "0.65rem" }}>
+             X {quantity} 
+        </span>
+                         )}
+      </div> 
+
 <div className="text-muted" style={{fontSize: ".75rem"}}>
     {formatCurrency(item.price)}
 </div>
+</div>
+
 <div>
     {formatCurrency(item.price  * quantity)}
+    </div>
+
     <Button variant="outline-danger" size="sm" onClick={()=> removeFromCart(item.id)}> 
     &times;     
     </Button>
-</div>
 
 
-</div>
 </Stack>  
  )
 }
